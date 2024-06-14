6e81268f54431bd0a6ad8e4383d179acfb5d1cea
adrian <adrian.vide.paris@gmail.com>
2024-06-14 20:37:12 +0200

    Add Card and grid with buttons to change layout

---
+++ app/components/Card/Card.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import { PropsWithChildren, createContext, useContext } from "react";
+
+type Product = {
+  type: string;
+  name: string;
+  price: number;
+  discount_price: number;
+  image: string;
+};
+
+type ProductContextType = {
+  product: Product;
+};
+
+const ProductContext = createContext<ProductContextType | undefined>(undefined);
+
+const useProduct = () => {
+  const context = useContext(ProductContext);
+  if (!context) {
+    throw new Error("useProduct must be used within a ProductProvider");
+  }
+  return context;
+};
+
+type CardProps = PropsWithChildren & {
+  product: Product;
+};
+
+const Card = ({ product, children }: CardProps) => {
+  console.log(product);
+  return (
+    <ProductContext.Provider value={{ product }}>
+      <div className="bg-gray-100 shadow-lg rounded-lg flex items-center justify-center flex-col">
+        {children}
+      </div>
+    </ProductContext.Provider>
+  );
+};
+
+const CardImage = () => {
+  const { product } = useProduct();
+  console.log(product);
+  return <Image src={product.image} alt={product.name} width={260} height={260}/>;
+};
+
+const CardName = () => {
+  const { product } = useProduct();
+  return <h2 className="text-xl font-bold">{product.name}</h2>;
+};
+
+const CardPrice = () => {
+  const { product } = useProduct();
+  return <p className="text-gray-500">{product.price}</p>;
+};
+
+const CardDiscountPrice = () => {
+  const { product } = useProduct();
+  return <p className="text-gray-500">{product.discount_price}</p>;
+};
+
+Card.Name = CardName;
+Card.Price = CardPrice;
+Card.Image = CardImage;
+Card.DiscountPrice = CardDiscountPrice;
+
+export default Card;
