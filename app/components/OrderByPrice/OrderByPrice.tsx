5d62712acb8e69111ad38482e4ea666cf9d70698
adrian <adrian.vide.paris@gmail.com>
2024-06-18 17:11:32 +0200

    Add OrderByPrice component for sorting products by price

---
+++ app/components/OrderByPrice/OrderByPrice.tsx
@@ -0,0 +1,28 @@
+type OrderByPriceProps = {
+  order: "asc" | "desc";
+  onOrderChange: (order: "asc" | "desc") => void;
+};
+
+const OrderByPrice: React.FC<OrderByPriceProps> = ({
+  order,
+  onOrderChange,
+}) => {
+  return (
+    <div className="">
+      <button
+        onClick={() => onOrderChange("asc")}
+        className={`symbol ${order === "asc" && "symbol__active"}`}
+      >
+        ˄
+      </button>
+      <button
+        onClick={() => onOrderChange("desc")}
+        className={`symbol ${order === "desc" && "symbol__active"}`}
+      >
+        ˅
+      </button>
+    </div>
+  );
+};
+
+export default OrderByPrice;
