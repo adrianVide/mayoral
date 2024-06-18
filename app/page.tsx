d97fe6f02f51a4706a359ebd26a1784f879783dc
adrian <adrian.vide.paris@gmail.com>
2024-06-18 17:11:24 +0200

    Styling for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import Card from "./components/Card/Card";
 import useWindowWidth from "./hooks/useWindowWidth";
 import useProductFilter from "./hooks/useProductFilter"; // Import the custom hook
 import products from "./HARDCODED/products.json";
+import OrderByPrice from "./components/OrderByPrice/OrderByPrice";
+import { Product } from "./types/product";
 
 export default function Home() {
   const [desktopColumns, setDesktopColumns] = useState(4);
   const [mobileColumns, setMobileColumns] = useState(1);
+  const [order, setOrder] = useState<"asc" | "desc">("asc");
   const isDesktop = useWindowWidth();
 
-  const desktopColumnRanges = [4, 5, 6]; // Columns for desktop: 4, 5, 6
-  const mobileColumnRanges = [1, 2, 3]; // Columns for mobile: 1, 2, 3
+  const desktopColumnRanges = [4, 5, 6];
+  const mobileColumnRanges = [1, 2];
 
-  const { filter, handleFilterChange, applyFilter } = useProductFilter(products);
+  const { filter, handleFilterChange, applyFilter } = useProductFilter();
 
   const handleIncrease = () => {
     if (isDesktop) {
@@ -37,14 +40,22 @@ export default function Home() {
     if (isDesktop) {
       setDesktopColumns((prev) => {
         const nextIndex = desktopColumnRanges.indexOf(prev) - 1;
-        return nextIndex >= 0 ? desktopColumnRanges[nextIndex] : desktopColumnRanges[0];
+        return nextIndex >= 0
+          ? desktopColumnRanges[nextIndex]
+          : desktopColumnRanges[0];
       });
     } else {
       setMobileColumns((prev) => {
         const nextIndex = mobileColumnRanges.indexOf(prev) - 1;
-        return nextIndex >= 0 ? mobileColumnRanges[nextIndex] : mobileColumnRanges[0];
+        return nextIndex >= 0
+          ? mobileColumnRanges[nextIndex]
+          : mobileColumnRanges[0];
       });
     }
+  };
+
+  const handleOrderChange = (newOrder: "asc" | "desc") => {
+    setOrder(newOrder);
   };
 
   const getGridTemplateColumns = () => {
@@ -53,43 +64,52 @@ export default function Home() {
       : `repeat(${mobileColumns}, minmax(0, 1fr))`;
   };
 
+  const applyOrder = (products: Product[]): Product[] => {
+    return [...products].sort((a, b) =>
+      order === "asc" ? a.price - b.price : b.price - a.price
+    );
+  };
+
   return (
-    <main className="p-4">
-      <div className="flex justify-center space-x-4 mb-4">
+    <main>
+      <div className="header-container">
         <input
           type="text"
           value={filter}
           onChange={(e) => handleFilterChange(e.target.value)}
-          placeholder="Filter by product name"
-          className="bg-gray-200 px-4 py-2 rounded-md"
+          placeholder="🔍 Buscar"
+          className="input-search"
         />
-        <button
-          onClick={handleDecrease}
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          -
-        </button>
-        <button
-          onClick={handleIncrease}
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          +
-        </button>
+        <div className="symbols-container">
+          <button
+            onClick={handleDecrease}
+            className="symbol"
+          >
+            -
+          </button>
+          <button
+            onClick={handleIncrease}
+            className="symbol"
+          >
+            +
+          </button>
+          <OrderByPrice order={order} onOrderChange={handleOrderChange} />
+        </div>
       </div>
       <div
         className="grid"
         style={{ gridTemplateColumns: getGridTemplateColumns() }}
       >
-        {applyFilter(products).map((product) => (
+        {applyOrder(applyFilter(products)).map((product) => (
           <Card key={product.id} product={product}>
             <Card.Image />
             <Card.Name />
             <Card.Price />
             <Card.DiscountPrice />
+            <Card.Button />
           </Card>
         ))}
       </div>
     </main>
   );
 }
-
